1f82d53e6a3de604479b84af9aa4b4fe3330edd4
김영민 <gim-yeongmin@gim-yeongmin-ui-MacBookPro.local>
2025-02-17 07:11:21 +0900

    맥북에서함

--- 03_control/src/edu/kh/control/practice/LoopPractice.java
+++ 03_control/src/edu/kh/control/practice/LoopPractice.java
@@ -86,6 +86,12 @@ public class LoopPractice {
 
 		System.out.print("첫번째 숫자 : ");
 		int num1 = sc.nextInt();
+		if(num1 < 0) { 
+			System.out.print("1 이상의 숫자를 입력해주세요");
+			System.out.print("\033[H\033[2J");
+			System.out.flush(); 
+			practice4(); 
+			} 
 		System.out.print("두번째 숫자 : ");
 		int num2 = sc.nextInt();
 
@@ -113,8 +119,7 @@ public class LoopPractice {
 	public void practice5() {
 
 		/*
-		 * 사용자로부터 입력 받은 숫자의 단을 출력하세요. 
-		 * 예) ===== 4단 ===== 4 * 1 = 4 4 * 2 = 8 4 * 3 = 12 4
+		 * 사용자로부터 입력 받은 숫자의 단을 출력하세요. 예) ===== 4단 ===== 4 * 1 = 4 4 * 2 = 8 4 * 3 = 12 4
 		 * * 4 = 16 4 * 5 = 20 4 * 6 = 24 4 * 7 = 28 4 * 8 = 32 4 * 9 = 36
 		 * 
 		 */
@@ -123,80 +128,114 @@ public class LoopPractice {
 
 		System.out.print("======" + dan + "단========");
 		System.out.println();
-		
+
 		for (int i = 1; i <= 9; ++i) {
-			System.out.printf("%d * %d = %d\n",dan,i,dan*i);
-			
+			System.out.printf("%d * %d = %d\n", dan, i, dan * i);
 
 		}
 
 	}
 
-	public void practice6(){
-		
+	public void practice6() {
+
 		/*
-		 * 사용자로부터 입력 받은 숫자의 단부터 9단까지 출력하세요.
-			단, 2~9를 사이가 아닌 수를 입력하면 
-			“2~9 사이 숫자만 입력해주세요”를 출력하세요.
-		 * */
+		 * 사용자로부터 입력 받은 숫자의 단부터 9단까지 출력하세요. 단, 2~9를 사이가 아닌 수를 입력하면 “2~9 사이 숫자만 입력해주세요”를
+		 * 출력하세요.
+		 */
 		System.out.print("단을 입력하시요 : ");
-		int dan =sc.nextInt();
-	
-		for (int i =dan;i<=9;++i) {
-			System.out.print("========"+i+"단========");
+		int dan = sc.nextInt();
+
+		for (int i = dan; i <= 9; ++i) {
+			System.out.print("========" + i + "단========");
 			System.out.println();
-			
-			for(int i2=1;i2<=9;++i2) {
-				System.out.printf("%d * %d = %d\n",i,i2,i*i2);
+
+			for (int i2 = 1; i2 <= 9; ++i2) {
+				System.out.printf("%d * %d = %d\n", i, i2, i * i2);
+			}
+
 		}
-		
-		
-		
-		}
-		
+
 	}
-	
-	public void practice7(){
+
+	public void practice7() {
 		/*
 		 * 다음과 같은 실행 예제를 구현하세요.
+		 * 
+		 * 
+		 * 정수 입력 : 4
+		 *
+		 **
+		 ***
+		 *****/
 
-			
-			정수 입력 : 4
-		 	*
-		 	**
-		 	***
-		 	*****/
-		
 		// -행(row)은 입력한 input 만큼
 		// 열(col)은 현재행(row)만큼 반복
-		
-		
+
 		System.out.print("정수 입력: ");
 		int num = sc.nextInt();
-		
-		char ch =  '*';
-		
-		for (int i =1;i<=num;  i++ ) { // 
-		   
-		for(int i2=1;i2<=i; i2++) {
-			System.out.print(ch);
-		}
-		System.out.println();// 이위치가 중요하다
-		
+
+		char ch = '*';
+
+		for (int i = 1; i <= num; i++) { //
+
+			for (int i2 = 1; i2 <= i; i2++) {
+				System.out.print(ch);
+			}
+			System.out.println();// 이위치가 중요하다
+
 		}
 	}
 
-	public void practice8(){
-		
+	public void practice8() {
+
+		/*
+		 * 메소드 명 : public void practice9(){} 다음과 같은 실행 예제를 구현하세요.
+		 * 
+		 * ex. 정수 입력 : 4
+		 ****
+		 ***
+		 **
+		 *
+		 */
+
+		System.out.print("정수 입력: ");
+		int num = sc.nextInt();
+
+		for (int i = num; i > 0; i--) {
+
+			for (int i2 = 0; i2 < i; ++i2) {
+				System.out.print('*');
+			}
+			System.out.println();
+
+		}
+
 	}
-	}
-		
-		
-		
-		
-	
+
+	public void practice9() {
+
+		/*
+		 * 메소드 명 : public void practice10(){} 다음과 같은 실행 예제를 구현하세요.
+		 * 
+		 * ex. 정수 입력 : 3
+		   *
+		   **
+		   ***
+		   **
+		   *
+		   *
+		  */
 
 	
+		System.out.print("정수 입력 : ");
+		int input = sc.nextint();
+		
+		for (int i = 0; i <= num; ++i)
 	
 	
- 
+	
+	
+	
+	
+	}
+}
--- 03_control/src/edu/kh/control/practice/Run2.java
+++ 03_control/src/edu/kh/control/practice/Run2.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	
 	LoopPractice lp = new LoopPractice();
 	
-	lp.practice7();
+	lp.practice4();
 	
 	
 
